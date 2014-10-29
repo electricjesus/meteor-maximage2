5bb0bac13e79d5e986f2a97b080dbd9130f18c98
Seth Malaki <seth.malaki@gmail.com>
2014-10-29 13:37:51 +0800

    Base API

--- electricjesus:maximage2.js
+++ electricjesus:maximage2.js
@@ -1,1 +1,20 @@
 // Write your package code here!
+Meteor.startup(function() {
+
+	(function($, undefined) {
+	  window.MaxImage = function(el, options) {
+		var element = el instanceof $ ? el : 
+				(typeof el === 'string' ? $(el) : false)
+			;
+		if(!element) {
+			throw "Must provide a string for a jQuery selector or a jQuery object";
+		} else {
+			if(!element.maximage) {
+				throw "Library error.";
+			} else {
+				element.maximage(options);
+			}
+		}
+	  };
+	})(jQuery);
+});
